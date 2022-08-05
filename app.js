75ad986d53685b650ef056a301aac92299178391
glaucerocha <glaucesr@gmail.com>
2022-08-04 21:09:53 -0300

    get Pessoas

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const express = require ("express");
-
 let app = express();
 
-console.log (app);
+let pessoas = [
+    {
+    id: 1,
+    nome: Glauce,
+    sobrenome: Rocha
+},
+{
+    id: 2,
+    nome: Rafael,
+    sobrenome: Losinno
+}
+
+]
+
+app.use(express.json())
+
+app.get ("/pessoas", (req, res) => {
+    res.send (cachorros)
+   
+})
+app.listen (3000,()=>{
+    console.log ("Rodando na porta 3000")
+
+})
+
